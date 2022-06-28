ffb63ef01930d71a1bb22a4c696d51b6943d7456
Presiyana Pavlova <107278477+PresiyanaPavlova@users.noreply.github.com>
2022-06-28 15:38:25 +0300

    fixed spacing on Start Day

--- src/test2.java
+++ src/test2.java
@@ -7,9 +7,8 @@ public class test2 {
     public static void printMonthTitle(int month) {
         System.out.println("     " + getMonthName(month) + " " + "2022");
         System.out.println("----------------------------");
-        System.out.println("S   M   T   W   T   F   S");
+        System.out.println("M   T   W   T   F  S  S");
     }
-
     public static String getMonthName(int month) {
         String monthName = null;
         switch (month) {
@@ -24,42 +23,38 @@ public class test2 {
             case 9: monthName = "September"; break;
             case 10: monthName = "October"; break;
             case 11: monthName = "November"; break;
-            case 12: monthName = "December";
+            case 12: monthName = "December"; break;
+            default:
+                System.out.println("Invalid input.");
         }
         return monthName;
     }
-
-    public static void printMonth(int month) {
-        printMonthTitle(month);
-       printMonthBody(month);
-    }
-
     public static void printMonthBody(int month) {
-        int startDay = getStartDay(month);
-        int numberOfDaysInMonth = getNumberOfDaysInMonth(month);
-        int repeatSpace = 7 - findDayOfTheWeek(month,1,2022);
-        System.out.print("   ".repeat(repeatSpace));                   //TODO: fix this spacing
-        for (int i = 1; i <= numberOfDaysInMonth; i++) {
+        int counterSpace = 1;
+        for (int i = findDayOfTheWeek(month, 1, 2022); i <= 7; i++) {
+            counterSpace++;
+        }
+
+        System.out.print("-".repeat(7 - counterSpace));                   //TODO: fix this spacing
+
+        for (int i = 1; i <= getNumberOfDaysInMonth(month); i++) {
             if (i < 10) {
                 System.out.print("  " + i);
             } else {
                 System.out.print(" " + i);
             }
-            if ((i + startDay) % 7 == 0) {
+            if ((i + getStartDay(month)) % 7 == 0) {
                 System.out.println();
             }
         }
     }
-
     public static int getStartDay (int month){
        int totalNumberOfDays = getNumberOfDaysInMonth(month);
-
-        return (totalNumberOfDays + (7 - findDayOfTheWeek(month,1, 2022)) % 7);
+        return (totalNumberOfDays + (findDayOfTheWeek(month,1, 2022)) % 7);
    }
-
    public static int findDayOfTheWeek(int month, int day, int year) {                      //determine which day of the week
         SimpleDateFormat simpleDateformat = new SimpleDateFormat("EEEE");
-        Date date = new GregorianCalendar(year, month - 1, day).getTime();     //see how to get rid of year, so it's2022 by default, and day 1 as default
+        Date date = new GregorianCalendar(year, month - 1, day).getTime();
         String dayOfTheWeek = simpleDateformat.format(date);
         int dayOfTheWeekInt = 0;
         switch (dayOfTheWeek.toUpperCase()){
@@ -71,7 +66,6 @@ public class test2 {
             case "SATURDAY": dayOfTheWeekInt = 6; break;
             case "SUNDAY": dayOfTheWeekInt = 7;
         }
-
         return dayOfTheWeekInt;
     }
     public static int getNumberOfDaysInMonth (int month){
@@ -86,13 +80,19 @@ public class test2 {
         return numberOfDaysInMonth;
     }
        public static void main(String[] args) {
-
-
            Scanner scan = new Scanner(System.in);
-           System.out.println("Enter month");
+           System.out.println("Welcome to your personal calendar! It looks empty, let's create an event.\nLet's see what month would you like to see?");
            int month = scan.nextInt();
-    printMonthTitle(month);
-    printMonthBody(month);
+           printMonthTitle(month);
+           printMonthBody(month);
        }
+       //starting the schedule
+    public static void userInputEventInfo (Scanner scan){
+        System.out.println("What date is your event?");
+        String date = scan.nextLine();
+        System.out.println("What time is your event?");
+        String time = scan.nextLine();
+
+    }
 
 }
